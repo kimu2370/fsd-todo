d1c154e05c3bfb6aef80512c493ca2851d71187a
seunghyunkim <martin.kim@legendaries.team>
2024-03-05 11:45:08 +0900

    fix: 프리티어 충돌 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'eslint:recommended',
     'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    "prettier/prettier": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
